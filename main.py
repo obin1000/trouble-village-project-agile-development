cf86c927747e0dc64823dc056fa493fc86456ae3
gianni1998 <gianniboy1998@gmail.com>
2018-03-20 13:46:20 +0100

    Wisselen van schermen geimplementeerd

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ print("Naam:", dorp.getName())
 print("Aantal inwoonders:", dorp.getPopulation())
 print("Aantal water:", dorp.getWater())
 print("Aantal hout:", dorp.getWood())
+
+from model.scene import *
+
+app = TroubleVillage()
+app.mainloop()
