38d779e0fcc1f099a032ed28f4ec52e09bc42456
Raphaelcenijn <cenynr@hva.nl>
2018-03-22 13:50:23 +0100

    Event classe die vertelt wat de status van het dorp is.

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ print("Naam:", dorp.getName())
 print("Aantal inwoonders:", dorp.getPopulation())
 print("Aantal water:", dorp.getWater())
 print("Aantal hout:", dorp.getWood())
+
+from model.event import *
+
+villageEvent = VillageEvent("", "...")
+
+print("Effect op het dorp:", villageEvent.getStatus())
+print("Effect op het dorp:", villageEvent.getEffect())
+
+from model.scene import *
+
+app = TroubleVillage()
+app.mainloop()
--- model/scene.py
+++ model/scene.py
@@ -1,1 +1,63 @@
-from Tkinter import *
+import tkinter as tk
+
+class TroubleVillage(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.container = tk.Frame(self)
+
+        self.title('Trouble Village')
+        self.geometry('800x500')
+        self.resizable(width=False, height=False)
+
+        self.container.pack(side="top", fill=None, expand=True)
+        self._frame = StartPage(master=self.container, controller=self)
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(master=self.container, controller=self)
+        self._frame.destroy()
+        self._frame = new_frame
+
+
+class StartPage(tk.Frame):
+    def __init__(self, master, controller):
+        tk.Frame.__init__(self, master)
+        self.controller = controller
+
+        start_label = tk.Label(self, text="This is the start page")
+        page_1_button = tk.Button(self, text="Open page Village",
+                                  command=lambda: controller.switch_frame(VillagePage))
+        page_2_button = tk.Button(self, text="Open page two",
+                                  command=lambda: controller.switch_frame(PageTwo))
+        start_label.pack(side="top", fill="x", pady=10)
+        page_1_button.pack()
+        page_2_button.pack()
+        self.pack()
+
+
+class VillagePage(tk.Frame):
+
+    def __init__(self, master, controller):
+
+        tk.Frame.__init__(self, master)
+        self.controller = controller
+
+        page_1_label = tk.Label(self, text="This is page Village")
+
+        start_button = tk.Button(self, text="Return to start page",
+                                 command=lambda: controller.switch_frame(StartPage))
+        page_1_label.pack(side="top", fill="x", pady=10)
+        start_button.pack()
+        self.pack()
+
+class PageTwo(tk.Frame):
+    def __init__(self, master, controller):
+        tk.Frame.__init__(self, master)
+        self.controller = controller
+
+        page_2_label = tk.Label(self, text="This is page two")
+        start_button = tk.Button(self, text="Return to start page",
+                                 command=lambda: controller.switch_frame(StartPage))
+        page_2_label.pack(side="top", fill="x", pady=10)
+        start_button.pack()
+        self.pack()
+
