c4dd4e038074c98498223bd16eb658b0bd73592d
Unknown <jorisvanelsland@gmail.com>
2018-03-22 14:19:50 +0100

    Auto stash before merge of "develop" and "origin/develop"
    Typo fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from model.village import *
 dorp = Village("Dorp 1", 100, 100, 50)
 
 print("Naam:", dorp.getName())
-print("Aantal inwoonders:", dorp.getPopulation())
+print("Aantal inwoners:", dorp.getPopulation())
 print("Aantal water:", dorp.getWater())
 print("Aantal hout:", dorp.getWood())
 
