0c09e91de7cf94c76512baac4d59273b28b9f978
Unknown <robinvonk789@gmail.com>
2018-05-17 15:35:40 +0200

    Ledjes codejes

--- driver/hokjesreader.py
+++ driver/hokjesreader.py
@@ -1,7 +1,5 @@
 import RPi.GPIO as GPIO
 
-import time
-
 
 class Resources:
     hout1 = 7
@@ -11,6 +9,14 @@ class Resources:
     water1 = 13
     water2 = 11
     water3 = 15
+
+    rood1 = 37
+    rood2 = 35
+    rood3 = 33
+
+    blauw1 = 40
+    blauw2 = 38
+    blauw3 = 36
     #Constructor for basic gpio settings
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
@@ -23,6 +29,14 @@ class Resources:
         GPIO.setup(Resources.water1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(Resources.water2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(Resources.water3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        GPIO.setup(Resources.rood1, GPIO.OUT)
+        GPIO.setup(Resources.rood2, GPIO.OUT)
+        GPIO.setup(Resources.rood3, GPIO.OUT)
+
+        GPIO.setup(Resources.blauw1, GPIO.OUT)
+        GPIO.setup(Resources.blauw2, GPIO.OUT)
+        GPIO.setup(Resources.blauw3, GPIO.OUT)
 
         #Enable interrupts for touch sensors
         GPIO.add_event_detect(Resources.hout1, GPIO.BOTH, callback=self.eenOfAndereCallback)
@@ -38,32 +52,32 @@ class Resources:
         #condition to seperate rising form falling
         if GPIO.input(channel):
             print("rising edge detected on" + str(channel))
-            if channel == 36:
-                GPIO.output(7, GPIO.LOW)
-            elif channel == 38:
-                GPIO.output(11, GPIO.LOW)
-            elif channel == 40:
-                GPIO.output(13, GPIO.LOW)
-            elif channel == 33:
-                GPIO.output(12, GPIO.LOW)
-            elif channel == 35:
-                GPIO.output(16, GPIO.LOW)
-            elif channel == 37:
-                GPIO.output(18, GPIO.LOW)
+            if channel == Resources.hout1:
+                GPIO.output(Resources.rood1, GPIO.LOW)
+            elif channel == Resources.hout2:
+                GPIO.output(Resources.rood2, GPIO.LOW)
+            elif channel == Resources.hout3:
+                GPIO.output(Resources.rood3, GPIO.LOW)
+            elif channel == Resources.water1:
+                GPIO.output(Resources.blauw1, GPIO.LOW)
+            elif channel == Resources.water2:
+                GPIO.output(Resources.blauw2, GPIO.LOW)
+            elif channel == Resources.water3:
+                GPIO.output(Resources.blauw3, GPIO.LOW)
         else:
             print("falling edge detected on" + str(channel))
-            if channel == 36:
-                GPIO.output(7, GPIO.HIGH)
-            elif channel == 38:
-                GPIO.output(11, GPIO.HIGH)
-            elif channel == 40:
-                GPIO.output(13, GPIO.HIGH)
-            elif channel == 33:
-                GPIO.output(12, GPIO.HIGH)
-            elif channel == 35:
-                GPIO.output(16, GPIO.HIGH)
-            elif channel == 37:
-                GPIO.output(18, GPIO.HIGH)
+            if channel == Resources.hout1:
+                GPIO.output(Resources.rood1, GPIO.HIGH)
+            elif channel == Resources.hout2:
+                GPIO.output(Resources.rood2, GPIO.HIGH)
+            elif channel == Resources.hout3:
+                GPIO.output(Resources.rood3, GPIO.HIGH)
+            elif channel == Resources.water1:
+                GPIO.output(Resources.blauw1, GPIO.HIGH)
+            elif channel == Resources.water2:
+                GPIO.output(Resources.blauw2, GPIO.HIGH)
+            elif channel == Resources.water3:
+                GPIO.output(Resources.blauw3, GPIO.HIGH)
 
     def getOccupiedResources(self):
         Occupied = [GPIO.input(Resources.hout1), GPIO.input(Resources.hout2), GPIO.input(Resources.hout3), GPIO.input(Resources.water1), GPIO.input(Resources.water2), GPIO.input(Resources.water3)]
